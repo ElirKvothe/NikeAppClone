df4b259ce62e20970e5e42f9ae35d4d34480accc
Alperen Yilmaz <114180379+ElirKvothe@users.noreply.github.com>
2024-11-02 21:56:08 +0300

    navigation tamamlandi, redux'a giris yapildi.

--- src/screens/ProductsScreen.js
+++ src/screens/ProductsScreen.js
@@ -1,32 +1,38 @@
 import { StyleSheet, Text, View, Image, FlatList, Pressable } from 'react-native';
-import products from '../data/products'
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+// import products from '../data/products';
 
 const ProductsScreen = () => {
-  const navigation = useNavigation()
-  
-    return (
-        <FlatList
-        data={products}
-        renderItem={({ item }) => (
-          <Pressable onPress={() => navigation.navigate('Product Details')} style={styles.itemContainer}>
-            <Image source={{uri: item.image,}} style={styles.image} />
-          </Pressable>
-        )}
-        numColumns={2}
-       />
-    )
+  const navigation = useNavigation();
+
+  const products = useSelector((state) => state.products.products);
+
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <Pressable
+          onPress={() => navigation.navigate('Product Details')}
+          style={styles.itemContainer}
+        >
+          <Image source={{ uri: item.image, }} style={styles.image} />
+        </Pressable>
+      )}
+      numColumns={2}
+    />
+  )
 }
 
 const styles = StyleSheet.create({
-    itemContainer: {
-      width: '50%',
-      padding: 1,
-    },
-    image: {
-      width: '100%',
-      aspectRatio: 1,
-    },
-  });
+  itemContainer: {
+    width: '50%',
+    padding: 1,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+});
 
 export default ProductsScreen;
