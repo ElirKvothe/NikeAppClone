5b52499aed9591dff19df7631d1d2decc2024f53
Alperen Yilmaz <114180379+ElirKvothe@users.noreply.github.com>
2024-11-02 05:08:45 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import ProductsScreen from './src/screens/ProductsScreen';
+import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      { /* <ProductsScreen /> */}
+      <ProductDetailsScreen />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
